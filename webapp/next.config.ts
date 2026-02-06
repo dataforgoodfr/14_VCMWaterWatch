4019f7f44c64a61808c2de220bea3335b53dbc23
Juliette Hoisnard <juho@netlight.com>
2026-02-06 11:54:00 +0100

    removed unused proxyredirect

--- webapp/next.config.ts
+++ webapp/next.config.ts
@@ -5,22 +5,6 @@ const nextConfig: NextConfig = {
 	reactStrictMode: false,
 	images: {
 		remotePatterns: [{ hostname: 'cdn.shadcnstudio.com' }, { hostname: 'noco-uploads.s3.fr-par.scw.cloud' }]
-	},
-	rewrites() {
-		const backend = process.env.NOCODB_URL
-
-		if (!backend) {
-			// If the backend URL is not set, do not add the rewrite to avoid invalid destination
-			console.warn('NOCODB_URL not defined; skipping API rewrite')
-			return []
-		}
-
-		return [
-			{
-				source: '/api/:path*', // Match requests starting with "/api"
-				destination: `${backend}/:path*` // Proxy to Backend
-			}
-		]
 	}
 }
 
