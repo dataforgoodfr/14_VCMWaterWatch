4d4bb4c686aa58f22f73ad1f3fd2ec6624c5d55a
Adrien <adrienquillet@gmail.com>
2026-02-26 03:47:30 +0100

    #20 | add design tokens (navy, aqua, gray) and button variants

--- webapp/components/ui/button.tsx
+++ webapp/components/ui/button.tsx
@@ -17,12 +17,17 @@ const buttonVariants = cva(
 					'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
 				secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
 				ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
-				link: 'text-primary underline-offset-4 hover:underline'
+				link: 'text-primary underline-offset-4 hover:underline',
+				outlineGhost:
+					'text-navy-800 text-md font-bold border bg-transparent shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
+				outlinePrimary:
+					'text-navy-800 text-md font-bold border bg-transparent group border-navy-800 hover:bg-aqua-100 hover:text-navy-900 items-center justify-between transition-colors'
 			},
 			size: {
 				default: 'h-9 px-4 py-2 has-[>svg]:px-3',
 				sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
 				lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
+				xl: 'p-3 rounded-xl has-[>svg]:px-4',
 				icon: 'size-9',
 				'icon-sm': 'size-8',
 				'icon-lg': 'size-10'
