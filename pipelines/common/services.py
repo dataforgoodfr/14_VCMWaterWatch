e23ea911c60f20aa5be6b75cbba3c7dfd00933eb
Nicolas Galler <ngaller@gmail.com>
2026-02-27 11:16:30 +0100

    #36 | unify env variable in python with the ones from web app

--- pipelines/common/services.py
+++ pipelines/common/services.py
@@ -8,16 +8,16 @@ dotenv.load_dotenv()
 
 def db_helper() -> DatabaseHelper:
     """Create a new DatabaseHelper instance."""
-    api_token = os.getenv("NOCODB_API_TOKEN")
+    api_token = os.getenv("NOCODB_TOKEN")
     if api_token is None:
-        raise ValueError("NOCODB_API_TOKEN is not set")
+        raise ValueError("NOCODB_TOKEN is not set")
 
     base_url = os.getenv("NOCODB_URL")
     if base_url is None:
         raise ValueError("NOCODB_URL is not set")
 
-    base_id = os.getenv("BASE_ID")
+    base_id = os.getenv("NOCODB_BASE_ID")
     if base_id is None:
-        raise ValueError("BASE_ID is not set")
+        raise ValueError("NOCODB_BASE_ID is not set")
 
     return DatabaseHelper(api_token=api_token, base_url=base_url, base_id=base_id)
