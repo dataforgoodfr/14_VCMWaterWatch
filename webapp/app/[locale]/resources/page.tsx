c77cce173da6d4ed0c613002669a1ddd247fb3be
Nicolas Galler <ngaller@gmail.com>
2026-04-18 12:09:29 +0200

    #54 | wire resources page with TabNav, ResourcesSection, MethodologySection

--- webapp/app/[locale]/resources/page.tsx
+++ webapp/app/[locale]/resources/page.tsx
@@ -1,8 +1,39 @@
+import { SectionSeparator } from '@/components/SectionSeparator'
+
+import { MethodologySection } from './components/MethodologySection'
+import { ResourcesSection } from './components/ResourcesSection'
+import { ResourceTabNav } from './components/ResourceTabNav'
+
 export default function ResourcesPage() {
 	return (
-		<main className='mx-auto w-full max-w-5xl px-6 py-16'>
-			<h1 className='text-3xl font-semibold text-gray-900'>Resources</h1>
-			<p className='mt-4 text-lg text-gray-600'>This page is under construction.</p>
+		<main className='container mx-auto px-4 py-16 md:px-8'>
+			{/* Page header */}
+			<div>
+				<h1 className='text-navy-800 font-[lexend] text-3xl font-semibold'>Resources</h1>
+				<div className='mt-6'>
+					<SectionSeparator />
+				</div>
+			</div>
+
+			{/* Sticky tab navigation */}
+			<div className='mt-6'>
+				<ResourceTabNav />
+			</div>
+
+			{/* Sections */}
+			<div className='mt-10 space-y-16'>
+				{/* Resources section */}
+				<section id='resources'>
+					<ResourcesSection />
+				</section>
+
+				<hr className='border-navy-200' />
+
+				{/* Methodology section */}
+				<section id='methodology'>
+					<MethodologySection />
+				</section>
+			</div>
 		</main>
 	)
 }
