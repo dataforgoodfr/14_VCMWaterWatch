4fbc14a568e48487d0fdd6dde88c8a40f00eb669
Nicolas Galler <ngaller@gmail.com>
2026-04-18 17:41:31 +0200

    #115 | whitelist locale param in countries API route

--- webapp/app/api/countries/[code]/route.ts
+++ webapp/app/api/countries/[code]/route.ts
@@ -7,7 +7,8 @@ export async function GET(request: Request, { params }: { params: Promise<{ code
 	const decoded = decodeURIComponent(code)
 
 	const { searchParams } = new URL(request.url)
-	const locale = searchParams.get('locale') ?? 'en'
+	const ALLOWED_LOCALES = new Set(['en', 'fr', 'de'])
+	const locale = ALLOWED_LOCALES.has(searchParams.get('locale') ?? '') ? searchParams.get('locale')! : 'en'
 
 	const result = await fetchCountryByCode(decoded, locale)
 
